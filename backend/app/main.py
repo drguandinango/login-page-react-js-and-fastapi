ed51c89945e83bf5c2b0fd9ff849f3e885960e4c
drguandinango <drguandinango@pucesi.edu.ec>
2025-04-26 22:19:05 -0500

    se ha generado el archivo de configuración para la coneccion de la bdd de Postgresql que se hará en el siguiente commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,33 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
+from app.config import db
+
 """
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import db
 from app.service.auth_service import generate_role
 """
-app = FastAPI()
+def init_app():
+    db.init()
 
-router = APIRouter()
+    app = FastAPI(
+        title= "Lemoncode21 App",
+        description= "Login Page",
+        version= "1"
+    )
+    
+    @app.on_event("startup")
+    async def starup():
+        await db.create_all()
+        
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()    
 
-@router.get("/")
-async def home():
-        return "welcome home"
+    return app
 
-app.include_router(router)
-
+app = init_app()
+ 
 def start():
         """Launched with 'poetry run start' at root level """
         uvicorn.run("app.main:app", host="localhost", port=8888, reload=True)
